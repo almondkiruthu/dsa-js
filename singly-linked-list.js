6c25a75023fe5f919d702a4390049f2db5c14698
Almond Kiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-08-17 19:23:55 +0300

    chore: :construction: create singly-ll- class add push and pop methods.

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -4,3 +4,42 @@ class Node {
     this.next = null;
   }
 }
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) return undefined;
+    let currentNode = this.head;
+    let newTail = currentNode;
+    while (currentNode.next) {
+      newTail = currentNode;
+      currentNode = currentNode.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return currentNode;
+  }
+}
