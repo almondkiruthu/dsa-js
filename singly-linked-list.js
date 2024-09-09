b0e4787077fb819874aafa3edcd7d04de409f014
Almond Kiruthu <almondkiruthu@gmail.com>
2024-09-09 20:42:07 +0300

    chore: :construction: add push and pop methods for the sll

---
+++ singly-linked-list.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+
+      this.length++;
+    }
+
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    let currentNode = this.head;
+    let newTail = currentNode;
+    while (currentNode.next) {
+      newTail = currentNode;
+      currentNode = currentNode.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return currentNode;
+  }
+}
