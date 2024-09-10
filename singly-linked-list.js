7c99a8b401ce81845ef84343fd2d5941cb100906
Almond Kiruthu <almondkiruthu@gmail.com>
2024-09-10 22:05:36 +0300

    "chore: :construction: add shift, unshift and get methods for the sll

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -20,9 +20,8 @@ class SinglyLinkedList {
     } else {
       this.tail.next = newNode;
       this.tail = newNode;
-
-      this.length++;
     }
+    this.length++;
 
     return this;
   }
@@ -44,4 +43,52 @@ class SinglyLinkedList {
 
     return currentNode;
   }
+  shift() {
+    if (!this.head) return undefined;
+    let oldHead = this.head;
+    this.head = oldHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return oldHead;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      let currentHead = this.head;
+      this.head = newNode;
+      this.head.next = currentHead;
+    }
+    this.length++;
+
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let currentNode = this.head;
+    let counter = 0;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+
+    return currentNode;
+  }
 }
+
+var list = new SinglyLinkedList();
+
+list.push(100);
+list.push(201);
+list.push(250);
+list.push(350);
+list.push(999);
+
+console.log(list);
+console.log(list.unshift(400));
+console.log(list.get(0));
