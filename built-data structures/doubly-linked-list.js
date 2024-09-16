13b92e2e3e89d9a173f08c8833249ca8b0e9b0bb
Almond Kiruthu <almondkiruthu@gmail.com>
2024-09-16 16:32:44 +0300

    feat: :sparkles: add unshift, set, get, insert and delete methods for the DLL.

--- built-data structures/doubly-linked-list.js
+++ built-data structures/doubly-linked-list.js
@@ -28,6 +28,7 @@ class DoublyLinkedList {
   }
 
   pop() {
+    if (!this.head) return undefined;
     let prevNode = this.tail.prev;
     let poppedNode = this.tail;
     if (this.length === 1) {
@@ -58,4 +59,87 @@ class DoublyLinkedList {
     this.length--;
     return removedNode;
   }
+
+  unshift(value) {
+    let newNode = new Node(value);
+    let prevHead = this.head;
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head = newNode;
+      newNode.next = prevHead;
+      prevHead.prev = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+  get(index) {
+    let count = 0;
+    let currentNode = this.head;
+    if (index <= this.length / 2) {
+      while (count !== index) {
+        currentNode = currentNode.next;
+        count++;
+      }
+    } else {
+      count = this.length - 1;
+      currentNode = this.tail;
+      while (count !== index) {
+        currentNode = currentNode.prev;
+        count--;
+      }
+    }
+    return currentNode;
+  }
+  set(index, value) {
+    let foundNode = this.get(index);
+    if (foundNode !== null || foundNode !== undefined) {
+      foundNode.value = value;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(value);
+    if (index === this.length) return !!this.push(value);
+
+    let newNode = new Node(value);
+
+    let nextNode = this.get(index);
+    let prevNode = nextNode.prev;
+
+    (prevNode.next = newNode), (newNode.prev = prevNode);
+    (newNode.next = nextNode), (nextNode.prev = newNode);
+
+    this.length++;
+
+    return true;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    let removedNode = this.get(index);
+    let nextNode = removedNode.next;
+    let prevNode = removedNode.prev;
+    prevNode.next = nextNode;
+    nextNode.prev = prevNode;
+
+    removedNode.prev = null;
+    removedNode.next = null;
+    this.length--;
+    return removedNode;
+  }
 }
+
+let list = new DoublyLinkedList();
+list.push("Harry");
+list.push("Ron");
+list.push("Hermione");
+
+console.log(list.unshift("Porter"));
