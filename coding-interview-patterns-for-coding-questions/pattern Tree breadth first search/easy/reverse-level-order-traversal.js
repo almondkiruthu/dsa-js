5fdee117f39113109252fc13e5b7d56aa0256ad9
Almond Kiruthu <almondkiruthu@gmail.com>
2024-10-30 23:38:07 +0300

    feat: :sparkles: add reverse binary tree level order traversal problem

---
+++ coding-interview-patterns-for-coding-questions/pattern Tree breadth first search/easy/reverse-level-order-traversal.js
@@ -0,0 +1,37 @@
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+function traverse(rootNode) {
+  const result = [];
+  if (!rootNode) return result;
+
+  const queue = [];
+  let currentNode = rootNode;
+  queue.push(currentNode);
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+      currentNode = queue.shift();
+      currentLevel.push(currentNode.value);
+      if (currentNode.left !== null) queue.push(currentNode.left);
+      if (currentNode.right !== null) queue.push(currentNode.right);
+    }
+
+    result.unshift(currentLevel)
+  }
+  return result;
+}
+
+const rootNode = new TreeNode(12);
+rootNode.left = new TreeNode(7);
+rootNode.right = new TreeNode(1);
+rootNode.left.left = new TreeNode(9);
+rootNode.right.left = new TreeNode(10);
+rootNode.right.right = new TreeNode(5);
+
+console.log(`Level order traversal: ${traverse(rootNode)}`);
