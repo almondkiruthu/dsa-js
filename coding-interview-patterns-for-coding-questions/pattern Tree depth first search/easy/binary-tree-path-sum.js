f9a7636f0d03230f5ce70086d67c4b04b5184a84
Almond Kiruthu <almondkiruthu@gmail.com>
2024-12-25 00:25:10 +0300

    feat: :sparkles: add binary tree path sum problem

--- coding-interview-patterns-for-coding-questions/pattern Tree depth first search/easy/binary-tree-path-sum.js
+++ coding-interview-patterns-for-coding-questions/pattern Tree depth first search/easy/binary-tree-path-sum.js
@@ -5,3 +5,26 @@ class TreeNode {
     this.right = null;
   }
 }
+
+function hasPath(root, sum) {
+  if (root === null) return false;
+
+  if (root.value === sum && root.left === null && root.right === null)
+    return true;
+
+  // recursively call to traverse the left and right sub-tree
+  return (
+    hasPath(root.left, sum - root.value) ||
+    hasPath(root.right, sum - root.value)
+  );
+}
+
+const root = new TreeNode(12);
+root.left = new TreeNode(7);
+root.right = new TreeNode(1);
+root.left.left = new TreeNode(9);
+root.right.left = new TreeNode(10);
+root.right.right = new TreeNode(5);
+
+console.log(`Tree has path: ${hasPath(root, 23)}`);
+console.log(`Tree has path: ${hasPath(root, 16)}`);
