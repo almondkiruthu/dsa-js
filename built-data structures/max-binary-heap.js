adef0d33de7330e3175a911d395278a8da5bbba3
Almond Kiruthu <almondkiruthu@gmail.com>
2025-01-20 21:35:03 +0300

    feat: :sparkles: create max binary extractMax method

--- built-data structures/max-binary-heap.js
+++ built-data structures/max-binary-heap.js
@@ -6,7 +6,16 @@ class MaxBinaryHeap {
     this.heap.push(value);
     this.bubbleUp();
   }
-  // percolate up
+  extractMax() {
+    const max = this.heap[0];
+    const lastElement = this.heap.pop();
+    if (this.heap.length > 0) {
+      this.heap[0] = lastElement;
+      this.bubbleDown();
+    }
+    return max;
+  }
+  // percolate up or bubble up
   bubbleUp() {
     let newValueIndex = this.heap.length - 1;
     let newValue = this.heap[newValueIndex];
@@ -24,6 +33,39 @@ class MaxBinaryHeap {
       newValueIndex = parentIndex;
     }
   }
+  // percolate down or bubble down
+  bubbleDown() {
+    let idx = 0;
+    const length = this.heap.length;
+    const element = this.heap[0];
+
+    while (idx < length) {
+      let leftChildIdx = idx * 2 + 1;
+      let rightChildIdx = idx * 2 + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.heap[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.heap[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.heap[idx] = this.heap[swap];
+      this.heap[swap] = element;
+      idx = swap;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
@@ -34,5 +76,6 @@ heap.insert(18);
 heap.insert(27);
 heap.insert(12);
 heap.insert(55);
+heap.insert(100)
 
 console.log(heap);
